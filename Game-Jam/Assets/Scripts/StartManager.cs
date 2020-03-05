426be0e77f0825f9ce03ab023c85617272fa7085
Dylan Cuevas <djc374@msstate.edu>
2020-03-05 02:17:46 -0600

    Added tiles and starting room for first section.

--- Game-Jam/Assets/Scripts/StartManager.cs
+++ Game-Jam/Assets/Scripts/StartManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+
+    public GameObject cam;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class StartManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cam.transform.position += new Vector3(Time.deltaTime / 1.5f, 0f, 0f);
+
+    }
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("First");
+        Debug.Log("Pressed");
     }
 }
